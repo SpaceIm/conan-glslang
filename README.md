11bae8ed8ad0e02c16363d79577cede9d3eb3085
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-04-15 13:36:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# conan-glslang
+
+## Package Status
+
+| Windows | Linux | macOS |
+|:-------:|:-----:|:-----:|
+|[![Build status](https://ci.appveyor.com/api/projects/status/r3nt9kcasecrhxk8/branch/testing%2F8.13.3559?svg=true)](https://ci.appveyor.com/project/SpaceIm/conan-glslang)|[![Build status](https://github.com/SpaceIm/conan-glslang/workflows/.github/workflows/conan.yml/badge.svg?branch=testing%2F8.13.3559)](https://github.com/SpaceIm/conan-glslang/actions?query=branch%3Atesting%2F8.13.3559)|[![Build Status](https://travis-ci.com/SpaceIm/conan-glslang.svg?branch=testing%2F8.13.3559)](https://travis-ci.com/SpaceIm/conan-glslang)|
+
+## Description
+
+[Conan.io](https://conan.io) package for [glslang](https://github.com/KhronosGroup/glslang).
---
+++ conanfile.py
@@ -0,0 +1,118 @@
+import os
+
+from conans import ConanFile, CMake, tools
+from conans.errors import ConanInvalidConfiguration
+
+class GlslangConan(ConanFile):
+    name = "glslang"
+    description = "Khronos-reference front end for GLSL/ESSL, partial front " \
+                  "end for HLSL, and a SPIR-V generator."
+    license = ["BSD-3-Clause", "NVIDIA"]
+    topics = ("conan", "glslang", "glsl", "hlsl", "spirv", "spir-v", "validation", "translation")
+    homepage = "https://github.com/KhronosGroup/glslang"
+    url = "https://github.com/conan-io/conan-center-index"
+    exports_sources = "CMakeLists.txt"
+    generators = "cmake"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "build_executables": [True, False],
+        "SPVRemapper": [True, False],
+        "hlsl": [True, False],
+        "with_spirv_tools": [True, False]
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+        "build_executables": True,
+        "SPVRemapper": True,
+        "hlsl": True,
+        "with_spirv_tools": False
+    }
+
+    _cmake = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.settings.compiler.cppstd:
+            tools.check_min_cppstd(self, 11)
+        if self.settings.os == "Windows" and self.options.shared:
+            raise ConanInvalidConfiguration("Current glslang shared library build is broken on Windows")
+
+    def requirements(self):
+        if self.options.with_spirv_tools:
+            raise ConanInvalidConfiguration("spirv-tools not yet available in conan-center-index")
+            self.requires.add("spirv-tools/2020.1")
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        os.rename(self.name + "-" + self.version, self._source_subfolder)
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.definitions["BUILD_EXTERNAL"] = False
+        self._cmake.definitions["SKIP_GLSLANG_INSTALL"] = False
+        self._cmake.definitions["ENABLE_SPVREMAPPER"] = self.options.SPVRemapper
+        self._cmake.definitions["ENABLE_GLSLANG_BINARIES"] = self.options.build_executables
+        self._cmake.definitions["ENABLE_GLSLANG_WEB"] = False
+        self._cmake.definitions["ENABLE_GLSLANG_WEB_DEVEL"] = False
+        self._cmake.definitions["ENABLE_EMSCRIPTEN_SINGLE_FILE"] = False
+        self._cmake.definitions["ENABLE_EMSCRIPTEN_ENVIRONMENT_NODE"] = False
+        self._cmake.definitions["ENABLE_HLSL"] = self.options.hlsl
+        self._cmake.definitions["ENABLE_OPT"] = self.options.with_spirv_tools
+        self._cmake.definitions["ENABLE_PCH"] = True
+        self._cmake.definitions["ENABLE_CTEST"] = False
+        self._cmake.definitions["USE_CCACHE"] = False
+        self._cmake.configure(build_folder=self._build_subfolder)
+        return self._cmake
+
+    def package(self):
+        self.copy("LICENSE.txt", dst="licenses", src=self._source_subfolder)
+        cmake = self._configure_cmake()
+        cmake.install()
+        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
+
+    def package_info(self):
+        # CMake Targets:
+        # - OGLCompiler
+        # - SPIRV
+        # - SPVRemapper
+        # - glslang
+        # - OSDependent
+        # - HLSL
+        self.cpp_info.names["cmake_find_package"] = "glslang"
+        self.cpp_info.names["cmake_find_package_multi"] = "glslang"
+
+        # - OGLCompiler has no dependency
+        # - SPIRV may depend on glslang
+        # - SPVRemapper has no dependency
+        # - glslang depends on OGLCompiler and OSDependent (and HLSL if ENABLE_HLSL)
+        # - OSDependent has no dependency (pthread on Linux)
+        # - HLSL has no dependency
+        self.cpp_info.libs = ["SPIRV", "glslang", "OGLCompiler", "OSDependent"]
+        if self.options.SPVRemapper:
+            self.cpp_info.libs.append("SPVRemapper")
+        if self.options.hlsl:
+            self.cpp_info.libs.append("HLSL")
+        if self.settings.os == "Linux":
+            self.cpp_info.system_libs.append("pthread") # for OSDependent
+        if self.options.build_executables:
+            self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
